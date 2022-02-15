2541fe68e57b5aa8c8ca565f50d39835e8b70b5d
Rafi Rahman <rafirahmann18@gmail.com>
2022-02-15 07:01:17 +0700

    edit endpoint get

--- routes/penduduk.route.js
+++ routes/penduduk.route.js
@@ -4,7 +4,7 @@ const express = require('express')
 const pendudukController = require('../controllers/penduduk.controller')
 const router = new express.Router();
 
-router.get("/", pendudukController.index)
+router.get("", pendudukController.index)
 router.post("/add", pendudukController.add)
 router.delete("/delete", pendudukController.delete)
 router.put("/update", pendudukController.put)
